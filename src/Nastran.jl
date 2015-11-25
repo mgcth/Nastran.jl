8a0768be645c29c8a8eea4216f33d53ccc42d802
Andrew Burkett <burkett.andrew@gmail.com>
2015-11-24 23:04:13 -0800

    Don't export card names

--- src/Nastran.jl
+++ src/Nastran.jl
@@ -5,7 +5,6 @@ import Base: show, +
 using ImmutableArrays
 
 export NastranDeck, GenericNastranDeck, NastranModel
-export CBEAM, CONM2, CELAS1, CQUADR, CTRIAR, CELAS2, RBE2, RBE3, CBUSH, CROD
 export CoordSet,get_coord,get_global_xyz
 export MassCG
 
